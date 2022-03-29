7d459c0b8c4fa01f079217cc43af5f19aaf94af0
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-03-29 15:50:11 +0300

    111

---
+++ js/test.js
@@ -0,0 +1,41 @@
+const x = 10;
+
+const y = 20;
+
+const z = x + y;
+
+const message = `Summ of ${x} and ${y} = ${z}`;
+console.log(x, y, z);
+console.log(message);
+console.log('X=', x, "Y=", y, "summ X+Y=", z);
+
+// const conf = confirm("Should the rus warship go fuck itself?");
+// console.log(conf);
+
+// let promT = prompt("Enter number");
+// console.log(promT);
+// console.log(typeof promT);
+
+// promT = Number(promT);
+// console.log(promT);
+// console.log(typeof promT);
+
+// let height = '2.0077px';
+
+// height = Number.parseFloat(height);
+// console.log(height);
+
+// let salary = 12.5557;
+// console.log(typeof salary);
+// console.log(salary);
+
+// salary = salary.toFixed(2);
+// console.log(typeof salary);
+// console.log(salary);
+
+// salary = Number(salary);
+// console.log(typeof salary);
+// console.log(salary);
+
+
+//salary=Number (salary.toFixed(2));
