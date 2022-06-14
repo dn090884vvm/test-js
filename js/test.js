8dc3d6a85417ecd5917067cf48694f88749cd500
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-06-14 22:07:32 +0300

    рпп
    впв

--- js/test.js
+++ js/test.js
@@ -1922,3 +1922,151 @@
 //   userList.innerHTML = markup;
 // }
 // import bootstrap from 'bootstrap';
+
+// const postToAdd = {
+//   author: 'Mango',
+//   body: 'CRUD is awesome',
+// };
+
+// const options = {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// const postToUpdate = {
+//   id: 101,
+//   body: 'CRUD is really awesome',
+// };
+
+// const options = {
+//   method: 'PATCH',
+//   body: JSON.stringify(postToUpdate),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// };
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postToUpdate.id}`, options)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log('ERROR' + error));
+
+// const postIdToDelete = 101;
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
+//   method: 'DELETE',
+// })
+//   .then(() => console.log('Post deleted'))
+//   .catch(error => console.log('Error:', error));
+
+// const postId = 100;
+
+// fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
+
+// const fetchFriends = () => {
+//   return fetch('my-api.com/me')
+//     .then(token => {
+//       return fetch(`my-api.com/profile?token=${token}`);
+//     })
+//     .then(user => {
+//       return fetch(`my-api.com/users/${user.id}/friends`);
+//     });
+// };
+
+// fetchFriends()
+//   .then(friends => console.log(friends))
+//   .catch(error => console.error(error));
+
+// const fetchUsers = async () => {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const users = await response.json();
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// fetchUsers();
+
+// const fetchUsers = async () => {
+//   const baseUrl = 'https://jsonplaceholder.typicode.com';
+//   const firstResponse = await fetch(`${baseUrl}/users/1`);
+//   const secondResponse = await fetch(`${baseUrl}/users/2`);
+//   const thirdResponse = await fetch(`${baseUrl}/users/3`);
+
+//   const firstUser = await firstResponse.json();
+//   const secondUser = await secondResponse.json();
+//   const thirdUser = await thirdResponse.json();
+
+//   console.log(firstUser, secondUser, thirdUser);
+// };
+
+// fetchUsers();
+
+// const fetchUsers = async () => {
+//   const baseUrl = 'https://jsonplaceholder.typicode.com';
+//   const userIds = [1, 2, 3];
+
+//   // 1. Создаём массив промисов
+//   const arrayOfPromises = userIds.map(async userId => {
+//     const response = await fetch(`${baseUrl}/users/${userId}`);
+//     console.log(response);
+//     return response.json();
+//   });
+
+//   // 2. Запускаем все промисы параллельно и ждем их завершения
+//   const users = await Promise.all(arrayOfPromises);
+//   console.log(users);
+// };
+
+// fetchUsers();
+
+const fetchUsersBtn = document.querySelector('.btn');
+const userList = document.querySelector('.user-list');
+
+fetchUsersBtn.addEventListener('click', async () => {
+  try {
+    const users = await fetchUsers();
+    renderUserListItems(users);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+async function fetchUsers() {
+  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  const userIds = [1, 2, 3, 4, 5];
+
+  const arrayOfPromises = userIds.map(async userId => {
+    const response = await fetch(`${baseUrl}/users/${userId}`);
+    return response.json();
+  });
+
+  const users = await Promise.all(arrayOfPromises);
+  return users;
+}
+
+function renderUserListItems(users) {
+  const markup = users
+    .map(
+      user => `<ul class="item">
+        <p><b>Name</b>: ${user.name}</p>
+        <p><b>Email</b>: ${user.email}</p>
+        <p><b>Company</b>: ${user.company.name}</p>
+      </ul>`
+    )
+    .join('');
+  userList.innerHTML = markup;
+}
